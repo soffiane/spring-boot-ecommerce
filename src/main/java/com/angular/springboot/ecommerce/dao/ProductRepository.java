5e08c9e925a402e36fa132e1355c6cf05f80bcaa
soffiane <sylar93120@hotmail.fr>
2025-10-06 16:15:51 +0200

    add CORS

--- src/main/java/com/angular/springboot/ecommerce/dao/ProductRepository.java
+++ src/main/java/com/angular/springboot/ecommerce/dao/ProductRepository.java
@@ -2,6 +2,9 @@ package com.angular.springboot.ecommerce.dao;
 
 import com.angular.springboot.ecommerce.entity.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
+//on autorise l'execution de code javascript depuis le navigateur en provenance de cette origine - CORS
+@CrossOrigin("http://localhost:4200")
 public interface ProductRepository extends JpaRepository<Product, Long> {
 }
